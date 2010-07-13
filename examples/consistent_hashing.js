5a3f5891462209982a6e5083316b74d616f9d03c
Arnout Kazemier <info@3rd-Eden.com>
2010-07-13 19:47:34 +0200

    Added examples and cleaned the source a bit more up. All exports are at the bottom.

---
+++ examples/consistent_hashing.js
@@ -0,0 +1,16 @@
+var sys = require( 'sys' ),
+		hashing = require( '../lib/hashring' ),
+		hashring = new hashing.hashRing(
+			[ '192.168.0.102:11212', '192.168.0.103:11212', '192.168.0.104:11212' ],
+			
+			// Weights are optional, but can be usefull if you have memcached server that can use allot of memory
+			{
+				'192.168.0.102:11212': 1,
+				'192.168.0.103:11212': 2,
+				'192.168.0.104:11212': 1
+			}
+		);
+		
+	sys.puts( hashring.get_node( "my-super-secret-cache-key" ) )
+	sys.puts( hashring.get_node( "hello-world" ) )
+	sys.puts( hashring.get_node( "my-super-secret-cache-key" ) )
