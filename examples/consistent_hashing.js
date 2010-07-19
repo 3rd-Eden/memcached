86d4bcc5443da5e64419b99fe8ce70bc48eb3e37
Arnout Kazemier <info@3rd-Eden.com>
2010-07-19 21:31:52 +0200

    Updated the example with the new constructor name. And added an example of using a different hashing alogrithm for the keys.

--- examples/consistent_hashing.js
+++ examples/consistent_hashing.js
@@ -1,6 +1,7 @@
 var sys = require( 'sys' ),
-	hashing = require( '../lib/hashring' ),
-	hashring = new hashing.hashRing(
+	HashRing = require( '../lib/hashring' ).HashRing;
+	
+var Ring = new HashRing(
 		[ '192.168.0.102:11212', '192.168.0.103:11212', '192.168.0.104:11212' ],
 		
 		// Weights are optional, but can be usefull if you have memcached server that can use allot of memory
@@ -10,7 +11,26 @@ var sys = require( 'sys' ),
 			'192.168.0.104:11212': 1
 		}
 	);
+
+// Return the server based on the key
+sys.puts( Ring.get_node( "my-super-secret-cache-key" ) );
+sys.puts( Ring.get_node( "hello-world" ) );
+sys.puts( Ring.get_node( "my-super-secret-cache-key" ) );
+
+// Different algorithms produce different hash maps. So choose wisely 
+var sha1Ring  = new HashRing(
+		[ '192.168.0.102:11212', '192.168.0.103:11212', '192.168.0.104:11212' ],
+		
+		// Weights are optional, but can be usefull if you have memcached server that can use allot of memory
+		{
+			'192.168.0.102:11212': 1,
+			'192.168.0.103:11212': 2,
+			'192.168.0.104:11212': 1
+		}, 
+		
+		'sha1' // optional algorithm for key hashing
+	);
 	
-sys.puts( hashring.get_node( "my-super-secret-cache-key" ) )
-sys.puts( hashring.get_node( "hello-world" ) )
-sys.puts( hashring.get_node( "my-super-secret-cache-key" ) )
+sys.puts( sha1Ring.get_node( "my-super-secret-cache-key" ) );
+sys.puts( sha1Ring.get_node( "hello-world" ) );
+sys.puts( sha1Ring.get_node( "my-super-secret-cache-key" ) );
