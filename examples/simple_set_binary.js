c72973aa71874ceff5e564308d1f07e868de3a03
Arnout Kazemier <info@3rd-Eden.com>
2010-07-22 13:22:27 +0200

    Added more examples, and yes.. also a hotchicks binary test! WEWT

---
+++ examples/simple_set_binary.js
@@ -0,0 +1,18 @@
+var	nMemcached = require( '../nMemcached' ).Client,
+	fs = require('fs'),
+	memcached;
+
+// connect to our memcached server on host 10.211.55.5, port 11211
+memcached = new nMemcached( "10.211.55.6:11211", { compression_threshold:10 } );
+
+fs.readFile( __dirname + '/binary/hotchicks.jpg', function( err, data ){
+	if( err ) console.error( err );
+	
+	
+	memcached.set( "hello_binary", data, 10000, function( err, result ){
+		if( err ) console.error( err );
+		
+		console.dir( result );
+		memcached.end(); // as we are 100% certain we are not going to use the connection again, we are going to end it
+	});	
+});
